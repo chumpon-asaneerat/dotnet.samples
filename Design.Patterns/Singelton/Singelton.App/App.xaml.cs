cc4fa3bdc2da60926b422f26016984c123a5ae34
chumpon-asaneerat <chumponsenate@gmail.com>
2022-01-28 12:03:27 +0700

    2022-01-28 #1 - Add Singleton.Common project - Add Singleton.App project - Update SQLite.Net.Sample   - Add related reference assemblies.

---
+++ Design.Patterns/Singelton/Singelton.App/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿#region Using
+
+using System.Windows;
+
+#endregion
+
+namespace Singelton
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+}
