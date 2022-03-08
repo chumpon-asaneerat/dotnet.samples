a09eee8ab6772a87c89c11cc3e99700e41e29c71
chumpon-asaneerat <chumponsenate@gmail.com>
2022-03-08 10:28:35 +0700

    2022-03-08 #14 - Wpf.WS.Server project   - Add RestStartUp class #1.

---
+++ Networks/RestAPI/Wpf.Rest.Server/RestStartUp.cs
@@ -0,0 +1,36 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+// Owin SelfHost
+using Owin;
+using Microsoft.Owin; // for OwinStartup attribute.
+using Microsoft.Owin.Hosting;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Validation;
+// Owin Authentication
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Text;
+// Swagger
+using System.Web.Http.Description;
+using System.Web.Http.Filters;
+
+//using Swashbuckle.Swagger;
+//using Swashbuckle.Application;
+
+#endregion
+
+namespace Wpf.Rest.Server
+{
+    public abstract class RestStartUp
+    {
+    }
+}
