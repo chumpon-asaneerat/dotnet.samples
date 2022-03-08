9cce113a9abd688d29e8d02596483a81e33ef442
chumpon-asaneerat <chumponsenate@gmail.com>
2022-03-08 10:24:17 +0700

    2022-03-08 #12 - Wpf.WS.Client project   - Update App.xaml, App.xaml.cs code. - Wpf.WS.Server project   - Update App.xaml, App.xaml.cs code.

--- Networks/WebSocket/Wpf.WS.Server/App.xaml.cs
+++ Networks/WebSocket/Wpf.WS.Server/App.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿#region Using
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+
+#endregion
 
 namespace Wpf.WS.Server
 {
@@ -13,5 +17,12 @@ namespace Wpf.WS.Server
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var window = new MainWindow();
+            window.Show();
+        }
     }
 }
