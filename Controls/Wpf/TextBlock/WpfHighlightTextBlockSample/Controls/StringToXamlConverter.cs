69ad35d9c263d35c5f6b8b894ed04735dfb97dda
chumpon-asaneerat <chumponsenate@gmail.com>
2022-11-10 07:20:17 +0700

    2022-11-10 #2 - WpfHighlightTextBlockSample project   - Implements feature #2.

---
+++ Controls/Wpf/TextBlock/WpfHighlightTextBlockSample/Controls/StringToXamlConverter.cs
@@ -0,0 +1,72 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Security;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+#endregion
+
+namespace WpfHighlightTextBlockSample.Controls
+{
+    class StringToXamlConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string input = value as string;
+            if (input != null)
+            {
+                var textBlock = new TextBlock();
+                textBlock.TextWrapping = TextWrapping.Wrap;
+                string escapedXml = SecurityElement.Escape(input);
+
+                while (escapedXml.IndexOf("|~S~|") != -1)
+                {
+                    //up to |~S~| is normal
+                    textBlock.Inlines.Add(new Run(escapedXml.Substring(0, escapedXml.IndexOf("|~S~|"))));
+                    //between |~S~| and |~E~| is highlighted
+                    textBlock.Inlines.Add(new Run(escapedXml.Substring(escapedXml.IndexOf("|~S~|") + 5,
+                                              escapedXml.IndexOf("|~E~|") - (escapedXml.IndexOf("|~S~|") + 5)))
+                    { FontWeight = FontWeights.Bold, Background = Brushes.Yellow });
+                    //the rest of the string (after the |~E~|)
+                    escapedXml = escapedXml.Substring(escapedXml.IndexOf("|~E~|") + 5);
+                }
+
+                if (escapedXml.Length > 0)
+                {
+                    textBlock.Inlines.Add(new Run(escapedXml));
+                }
+                return textBlock;
+            }
+
+            return null;
+        }
+        /// <summary>
+        /// ConvertBack.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("This converter cannot be used in two-way binding.");
+        }
+
+    }
+}
