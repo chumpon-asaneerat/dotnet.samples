c7282b8c52afc3d2873d41eca33c1543f636ef2f
chumpon-asaneerat <chumponsenate@gmail.com>
2022-12-17 10:16:54 +0700

    2022-12-17 #5 - WpfDataBindingAttachPropertySample project.   - Implements #4

--- Controls/Wpf/AttachProperty/WpfDataBindingAttachPropertySample/MainWindow.xaml.cs
+++ Controls/Wpf/AttachProperty/WpfDataBindingAttachPropertySample/MainWindow.xaml.cs
@@ -36,12 +36,51 @@ namespace WpfDataBindingAttachPropertySample
         }
 
         #endregion
+
+        #region Load/Unloaded
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Run();
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdCommit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private Example testObj = new Example();
+
+        #endregion
+
+        #region Private Methods
+
+        private void Run()
+        {
+            this.DataContext = testObj;
+        }
+
+        #endregion
     }
+
+    #region Example class
 
     public class Example : INotifyPropertyChanged
     {
         private string _text = string.Empty;
-
 
         private void RaisePropertyChanged(string propertyName)
         {
@@ -61,7 +100,8 @@ namespace WpfDataBindingAttachPropertySample
             }
         }
 
-
         public event PropertyChangedEventHandler PropertyChanged;
     }
+
+    #endregion
 }
